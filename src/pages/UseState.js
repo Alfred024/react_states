318d8a014218604a61190e58fccea0ef3f4a2888
Alfredo <21030761@itcelaya.edu.mx>
2023-07-19 09:36:31 -0600

    Adding state components in a normal Component

--- src/pages/UseState.js
+++ src/pages/UseState.js
@@ -3,38 +3,46 @@ import React from "react";
 const SECURITY_CODE = 'lola';
 
 function UseState() {
-    const [typed, setTyped] = React.useState('');
-    const [error, setError] = React.useState(false);
-    const [loading, setLoading] = React.useState(false);
+    const [state, setState] = React.useState({
+        typed: '',
+        loading: false,
+        error: false,
+    });
+
+    // const [typed, setTyped] = React.useState('');
+    // const [error, setError] = React.useState(false);
+    // const [loading, setLoading] = React.useState(false);
 
     React.useEffect(() =>{
-        setError(false);
-        if(loading){
+        if(state.loading){
+            setState({...state, error: false});
             setTimeout(() =>{
-                if(typed !== SECURITY_CODE){
-                    setError(true);
+                if(state.typed !== SECURITY_CODE){
+                    setState({...state, error: true});
                 }
-                setLoading(false);
+                setState({...state, loading: false});
             }, 2000);
         }
-    }, [loading])
+    }, [state.loading])
 
     return(
       <div className="flex flex-col justify-center border border-x-red-500 p-4">
         <h2>Eliminar UseState</h2>
         <p>Por favor,escribe el código de seguridad.</p>
 
-        {error && (<p>Hay un error</p>)}
-        {loading && (<p>Cargando...</p>)}
+        {(state.error) && (<p>Hay un error</p>)}
+        {state.loading && (<p>Cargando...</p>)}
 
         <input 
             placeholder="Código de seguridad"
             onChange={(event) => {
-                setTyped(event.target.value);
+                setState({...state, typed: event.target.value});
             }}/>
 
         <button
-            onClick={()=>{setLoading((state) => !state)}}
+            onClick={()=>{
+                setState({...state, loading: true});
+            }}
         >Comprobar</button>
       </div>  
     );
